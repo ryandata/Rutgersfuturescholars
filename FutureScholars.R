38b4c45e35cb2d9b1a31b0481ad985680be541d9
Ryan Womack <ryan@womack.mn>
2019-08-02 00:19:23 -0400

    pyramid

--- FutureScholars.R
+++ FutureScholars.R
@@ -152,84 +152,228 @@ brainycolleges <- brainycolleges[order(brainycolleges$SAT_AVG),]
 brainycolleges[,"INSTNM"]
 write.csv(brainycolleges, file="brainycolleges.csv")
 
-#### DATA VISUALIZATION AND WEB APPS
+###############
+# Evaluating Population
+# Functions using the idbr package
+# developed for Rutgers Future Scholars
+# by Ryan Womack
+# 2017-07-12
+###############
+
+# see 
+# https://www.census.gov/data/developers/data-sets/international-database.html
+# for information of variables used by idb
+#
+# look up FIPS country codes here
+# https://en.wikipedia.org/wiki/List_of_FIPS_country_codes
+#
+# FIPS are standardized two-letter country codes and are 
+# the easiest way to input country information for idbr
 
 
-##### Editing Data for use with GoogleVis
-##### Not done in class
-##### This merges all years of Scorecard Data into one file
+#####
+# install required packages
+#####
 
-# this should be edited to match your own directory
-setwd("/home/ryan/Downloads/college-scorecard-raw-data-030216/")
-library(reshape)
+install.packages("idbr", dependencies=TRUE)
+install.packages("ggthemes", dependencies=TRUE)
+install.packages("animation", dependencies=TRUE)
+install.packages("dplyr", dependencies=TRUE)
 
-# this step is necessary to process the files and add a year marker to them
-mynames<-paste("merged_",seq.int(1996,2013),"_PP.csv",sep="")
-myChanges<-c("1996"="YEAR", "1997"="YEAR", "1998"="YEAR", "1999"="YEAR", "2000"="YEAR",
-             "2001"="YEAR", "2002"="YEAR", "2003"="YEAR", "2004"="YEAR", "2005"="YEAR",
-             "2006"="YEAR", "2007"="YEAR", "2008"="YEAR", "2009"="YEAR", "2010"="YEAR",
-             "2011"="YEAR", "2012"="YEAR", "2013"="YEAR")
+#####
+# load required packages
+#####
 
-# creates new files for each year, with a year column added
-for (i in seq(1:18))
+library(idbr) # devtools::install_github('walkerke/idbr')
+library(ggplot2)
+library(animation)
+library(dplyr)
+library(ggthemes)
+
+######
+# setup
+# you must request an Census API key for this package to work
+# get your key at
+# http://api.census.gov/data/key_signup.html
+
+# idb_api_key('you must put your Census API key in here for this to work')
+idb_api_key('72e24b879a932f21b4ec758546dd8a5193282bec')
+
+######
+
+# fertility
+
+# function to compare fertility rates
+# input two country codes, start year, end year
+# put quotes around country codes
+
+fertility <- function(a,b,c,d)
 {
-  myname<-mynames[i]
-  mydata<-read.csv(myname, as.is=TRUE, na.strings="NULL")
-  j<<-i+1995
-  mydata<-merge(j,mydata)
+  country_one<-a
+  country_two<-b
+  start_year<-c
+  end_year<-d
   
-  write.csv(mydata, file=myname)
+  ssr_df <- idb5(c(country_one,country_two), year=c:d, variable='TFR' ,country_name = TRUE)
+  
+  ggplot(ssr_df, aes(x = time, y=TFR, color=NAME)) + 
+    geom_line(size = 1) + 
+    ylab('Total Fertility Rate') + 
+    xlab('Year') + 
+    theme(legend.title = element_blank(), 
+          legend.position = "bottom") 
 }
 
-# merging all files happens outside of R
-# In linux, go into terminal environment and cat files together with
-# cat *.csv > mergedfile.csv
+######
 
-# Now we read in the big data file and select only the 4-year colleges
-# using the subset command to look for matches
+# death rate
 
-mydata<-read.csv("mergedfile.csv")
-mydata2<-subset(mydata,HIGHDEG==3|HIGHDEG==4)
+# function to compare death rates
+# input two country codes, start year, end year
+# put quotes around country codes
 
-# we also want to filter the columns to only a few of the most
-# relevant ones
-# this is done with the subset+select combination below
-
-mydata3<-subset(mydata2,select=c(x,INSTNM,CITY,STABBR,SATMTMID,SATVRMID,SATWRMID,
-                                 ADM_RATE,UGDS,UGDS_WHITE,UGDS_BLACK,UGDS_ASIAN,UGDS_HISP,
-                                 GRAD_DEBT_MDN_SUPP,pct25_earn_wne_p6,PCTFLOAN,md_earn_wne_p10))
-
-# just cleaning up here, and making sure YEAR is numeric
-mydata3<-rename(mydata3, c(x="YEAR"))
-mydata3$YEAR<-as.numeric(mydata3$YEAR)
-for (i in seq.int(5,17))
+death <- function(a,b,c,d)
 {
-  mydata3[,i]<-as.numeric(mydata3[,i])
+  country_one<-a
+  country_two<-b
+  start_year<-c
+  end_year<-d
+  
+  ssr_df <- idb5(c(country_one,country_two), year=c:d, variable='CDR' ,country_name = TRUE)
+  
+  ggplot(ssr_df, aes(x = time, y=CDR, color=NAME)) + 
+    geom_line(size = 1) + 
+    ylab('Crude Death Rate') + 
+    xlab('Year') + 
+    theme(legend.title = element_blank(), 
+          legend.position = "bottom") 
+}
+
+######
+
+# growth rate
+
+# function to compare growth rates
+# input two country codes, start year, end year
+# put quotes around country codes
+
+growth <- function(a,b,c,d)
+{
+  country_one<-a
+  country_two<-b
+  start_year<-c
+  end_year<-d
+  
+  ssr_df <- idb5(c(country_one,country_two), year=c:d, variable='GR' ,country_name = TRUE)
+  
+  ggplot(ssr_df, aes(x = time, y=GR, color=NAME)) + 
+    geom_line(size = 1) + 
+    ylab('Growth Rate') + 
+    xlab('Year') + 
+    theme(legend.title = element_blank(), 
+          legend.position = "bottom") 
 }
 
 
-# removing duplicates (a problem for googleVis)
-mydata3$NAME<-paste(mydata3$INSTNM,mydata3$CITY,mydata3$STABBR,sep="-") 
+######
 
-names(mydata3)
+# population
 
-# and exporting the file for later use
-write.csv(mydata3, file="select_college_data_1996_2013.csv")
+# function to compare total population
+# input two country codes, start year, end year
+# put quotes around country codes
 
-# create New Jersey only dataset
-mydata4<-subset(mydata3, STABBR=="NJ")
-write.csv(mydata4, file="NJ_only_select_college_data_1996_2013.csv")
+population <- function(a,b,c,d)
+{
+  country_one<-a
+  country_two<-b
+  start_year<-c
+  end_year<-d
+  
+  ssr_df <- idb5(c(country_one,country_two), year=c:d, variable='POP' ,country_name = TRUE)
+  
+  ggplot(ssr_df, aes(x = time, y=POP, color=NAME)) + 
+    geom_line(size = 1) + 
+    ylab('Total Population') + 
+    xlab('Year') + 
+    theme(legend.title = element_blank(), 
+          legend.position = "bottom") 
+}
 
-#read it in again
-mydata4<-read.csv("NJ_only_select_college_data_1996_2013.csv")
-##### Run GoogleVis
 
-library(googleVis)
-G <- gvisMotionChart(mydata4, idvar="NAME", timevar="YEAR")
-plot(G)
+#####
+# Note we can also retrieve sets of concepts like this
+ssr_df <- idb5(c('RS','US'), year=1990:2015, concept='Components of population growth' ,country_name = TRUE)
+ssr_df <- idb5(c('RS'), year=1990:2015, concept='Fertility rates' ,country_name = TRUE)
 
-#### Working with Shiny
 
-#### Shiny files (app.R, ui.R, server.R) are stored separately
-#### load app.R for a project that browses individual points
-#### load ui.R and server.R for a scatterplot that lets you select variables
+
+##############################
+# Population Pyramid
+# 
+# Create a population pyramid!
+##############################
+
+pyramid2<-function(countrycode,countryname,startyear,endyear)
+{
+  
+  # countrycode<-c('UK')
+  # countryname<-"UK"
+  # startyear=2010
+  # endyear=2050
+  
+  
+  
+  male <- idb1(countrycode, startyear:endyear, sex = 'male') %>%
+    mutate(POP = POP * -1,
+           SEX = 'Male')
+  
+  female <- idb1(countrycode, startyear:endyear, sex = 'female') %>%
+    mutate(SEX = 'Female')
+  
+  countrydata <- rbind(male, female) %>%
+    mutate(abs_pop = abs(POP))
+  
+  # Animate it with a for loop
+  
+  saveHTML({
+    
+    for (i in startyear:endyear) {
+      
+      title <- as.character(i)
+      
+      year_data <- filter(countrydata, time == i)
+      
+      g1 <- ggplot(year_data, aes(x = AGE, y = POP, fill = SEX, width = 1)) +
+        coord_fixed() +
+        coord_flip() +
+        annotate('text', x = 98, y = -800000,
+                 label = 'Data: US Census Bureau IDB; idbr R package', size = 3) +
+        geom_bar(data = subset(year_data, SEX == "Female"), stat = "identity") +
+        geom_bar(data = subset(year_data, SEX == "Male"), stat = "identity") +
+        scale_y_continuous(breaks = seq(-1000000, 1000000, 500000),
+                           labels = paste0(as.character(c(seq(1, 0, -0.5), c(0.5, 1))), "m"),
+                           limits = c(min(countrydata$POP), max(countrydata$POP))) +
+        theme_economist(base_size = 14) +
+        scale_fill_manual(values = c('#ff9896', '#d62728')) +
+        ggtitle(paste0('Population structure of ',countryname, title)) +
+        ylab('Population') +
+        xlab('Age') +
+        theme(legend.position = "bottom", legend.title = element_blank()) +
+        guides(fill = guide_legend(reverse = TRUE))
+      
+      print(g1)
+      
+    }
+    
+  }, htmlfile="index.html")
+  
+  
+}
+
+# for example
+death("UK","US", 2010, 2050)
+fertility("UK","US", 2010, 2050)
+growth("UK","US", 2010, 2050)
+population("UK","US", 2010, 2050)
+pyramid2("UK","UK", 2010, 2050)
